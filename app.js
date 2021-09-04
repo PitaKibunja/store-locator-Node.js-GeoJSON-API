ab5db56ec6d0b7b18c7168d283563b465d272631
dapintechnologies <dapintech@gmail.com>
2021-09-04 15:46:23 +0300

    saving the data to the db

---
+++ app.js
@@ -0,0 +1,29 @@
+const path=require('path')
+const express=require('express')
+const dotenv=require('dotenv')
+const cors=require('cors')
+const connectDB=require('./config/db')
+//load env vars
+dotenv.config({ path: './config/config.env' })
+
+//connect to the database
+connectDB()
+
+const app = express()
+
+const storesRouter=require('./routes/stores')
+
+//body parser middleware
+app.use(express.json())
+
+//Enable cors
+app.use(cors())
+
+app.use('/api/v1/stores',storesRouter)
+
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
---
+++ controllers/stores.js
@@ -0,0 +1,43 @@
+//import the model
+const Store=require('../models/Store')
+
+//Get all stores
+//GET /api/v1/stores
+//access public
+
+exports.getStores =async (req, res, next) => {
+    try {
+        const stores = await Store.find()
+        
+        return res.status(200).json({
+            success: true,
+            count: stores.length,
+            data:stores
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            error: 'Server error'
+        })
+    }
+}
+
+//create a store
+//route POST /api/v1/stores
+//access Public
+
+exports.addStore = async (req, res, next) => {
+    try {
+        const store = await Store.create(req.body)
+        return res.status(200).json({
+            success: true,
+            data:store
+        })
+    } catch (err) {
+        console.error(err)
+        if (err.code === 11000) {
+            return res.status(400).json({error:'This store already exists!'})
+        }
+        res.status(500).json({error:'Server error'})
+    }
+}
